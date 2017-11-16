9f611d012405dd208ab1d468acfa658084060134
StochitaCatalin <33554428+StochitaCatalin@users.noreply.github.com>
2017-11-16 02:36:15 +0200

    Implementare vizualizator de noduri
    Using Algorithm for Drawing Trees made by Rachel Lim (https://rachel53461.wordpress.com)

---
+++ MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace FormulaBineFormata
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // treePanel
+            // 
+            this.treePanel.BackColor = System.Drawing.Color.Transparent;
+            this.treePanel.Location = new System.Drawing.Point(0, 0);
+            this.treePanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.treePanel.Name = "treePanel";
+            this.treePanel.Size = new System.Drawing.Size(230, 230);
+            this.treePanel.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.treePanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel treePanel;
+    }
+}
+
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -21,7 +22,22 @@ namespace FormulaBineFormata
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        string contectori = "¬∧∨→↔";
+        public class Nod
+        {
+            public List<Nod> nods = new List<Nod>();
+            public List<char> ch = new List<char>();
+        }
+        Nod MyNod;
+        string text;
+        List<int> posindex = new List<int>();
+        int pos = 0;
+        int cnt = 0;
+        string conectori = "¬∧∨→↔";
+        string conector = "∧∨→↔";
+        bool Isprop(char ch)
+        {
+            return (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z');
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -29,7 +45,7 @@ namespace FormulaBineFormata
 
         string tostrict(string text)
         {
-            foreach (char ch in contectori)
+            foreach (char ch in conectori)
             {
                 for (int i = text.Length - 1; i >= 0; i--)
                 {
@@ -49,14 +65,16 @@ namespace FormulaBineFormata
                                     cnt--;
                                     if (cnt == 0)
                                     {
-                                        if (text[j + 1] == ')')
+                                        if ((j+1) < text.Length && text[j + 1] == ')')
                                             ep[1] = true;
-                                        ins[1] = j;
+                                        ins[1] = j+1;
                                         //text = text.Insert(j, ")");
                                         break;
                                     }
                                 }
                             }
+                            //if (cnt != 0)
+                            //    ins[1] = text.Length;
                         }
                         else
                         {
@@ -79,7 +97,7 @@ namespace FormulaBineFormata
                                         cnt--;
                                         if (cnt == 0)
                                         {
-                                            if (text[j - 1] == '(')
+                                            if ((j-1) >= 0 && text[j - 1] == '(')
                                                 ep[0] = true;
                                             ins[0] = j;
                                             //text = text.Insert(j, "(");
@@ -103,20 +121,132 @@ namespace FormulaBineFormata
                             ins[0] = i;
                             //text = text.Insert(i, "(");
                         }
-                        if(!ep[0] || !ep[1])
+                        if (!(ep[0] && ep[1]))
                         {
                             text = text.Insert(ins[1], ")");
+                            posindex.Insert(ins[1], -1);
                             text = text.Insert(ins[0], "(");
+                            posindex.Insert(ins[0], -1);
+                            //for (int n = ins[0] + 1; n < ins[1]; n++)
+                            //    posindex[n] = -1;
                         }
+                        //else
+                        //{
+                         //   posindex[ins[0]-1] = -1;
+                         //   posindex[ins[1]] = -1;
+                        //}
+                        //else
+                        //    for (int n = ins[0] - 1; n <= ins[1]; n++)
+                        //        posindex[n] = -1;
                     }
                 }
             }
             return text;
         }
-
+        Nod createnod(char cha)
+        {
+            Nod tmp = new Nod();
+            tmp.ch.Add(cha);
+            return tmp;
+        }
+        List<Nod> Compressnods(List<Nod> nc,int nr = 0)
+        {
+            int j = 0;
+            if (nr == 0)
+                nr = nc.Count - 1;
+            for (int i = nc.Count - 2; i >= 0 && j < nr; i--,j++)
+            {
+                nc[i].nods.Add(nc[i + 1]);
+                nc.RemoveAt(i + 1);
+            }
+            return nc;
+        }
+        Nod tonod()
+        {
+            List<char> need = new List<char>();
+            List<Nod> buffernods = new List<Nod>();
+            need.Add('p');
+            //(P→(Q∧(S→T)))
+            //
+            while (pos < text.Length)
+            {
+                if(need.Count == 0)
+                {
+                    Console.WriteLine("Not expected anything");
+                    break;
+                }
+                if (need[need.Count - 1] == 'p')
+                {
+                    if (Isprop(text[pos]))
+                    {
+                        need.RemoveAt(need.Count - 1);
+                        if (buffernods.Count > 0)
+                            buffernods[buffernods.Count - 1].nods.Add(createnod(text[pos]));
+                        else
+                            buffernods.Add(createnod(text[pos]));
+                    }
+                    else if (text[pos] == '(')
+                    {
+                        need.RemoveAt(need.Count - 1);
+                        need.Add(')');
+                        need.Add('p');
+                        need.Add('c');
+                        need.Add('p');
+                        buffernods.Add(createnod(text[pos]));
+                    }
+                    else if(text[pos] == conectori[0])
+                    {
+                        if (need[need.Count - 2] == 'c')
+                        {
+                            need.RemoveAt(need.Count - 1);
+                            need.RemoveAt(need.Count - 1);
+                            buffernods[buffernods.Count - 1].ch.Add(text[pos]);
+                        }
+                    }
+                }
+                else if (need[need.Count - 1] == text[pos])
+                {
+                    need.RemoveAt(need.Count - 1);
+                    buffernods[buffernods.Count - 1].ch.Add(')');
+                    buffernods = Compressnods(buffernods, 1);
+                }
+                else if(need[need.Count - 1] == 'c' && conector.Contains(text[pos]))
+                {
+                    need.RemoveAt(need.Count - 1);
+                    buffernods[buffernods.Count - 1].ch.Add(text[pos]);
+                }
+                else
+                {
+                    Console.WriteLine("Problema " + text[pos]);
+                    break;
+                }
+                pos++;
+            }
+            Nod tmp = buffernods[0];
+            return tmp;
+        }
+        private void reset()
+        {
+            pos = 0;
+            cnt = 0;
+            posindex.Clear();
+            for (int i = 0; i < text.Length; i++)
+                posindex.Add(i);
+        }
         private void Formula_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Console.WriteLine(tostrict(formula.Text));
+            text = formula.Text;
+            reset();
+            
+            text = tostrict(text);
+            Console.WriteLine(text);
+            MyNod = tonod();
+        }
+
+        private void aratanoduri_Click(object sender, RoutedEventArgs e)
+        {
+            MainForm form = new MainForm(MyNod);
+            form.ShowDialog();
         }
     }
 }
